849190cfa284e027779a54fb348b487eef4db1c5
sarhov <sarhov@sarhov.com>
2014-12-28 18:02:43 +0400

    update

--- config.rb
+++ config.rb
@@ -1,6 +1,3 @@
-# Require any additional compass plugins here.
-add_import_path "bower_components/foundation/scss"
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css/"
@@ -23,8 +20,3 @@ javascripts_dir = "js"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
-if Gem.loaded_specs["sass"].version >= Gem::Version.create('3.4')
-  warn "You're using Sass 3.4 or higher to compile Foundation. This version causes CSS classes to output incorrectly, so we recommend using Sass 3.3 or 3.2."
-  warn "To use the right version of Sass on this project, run \"bundle\" and then use \"bundle exec compass watch\" to compile Foundation."
-end
